9f30badb6eb6c5dfe7890443054c8f85b0f8b4b9
Luis Rondon <leduardo.rondon@gmail.com>
2022-10-18 19:56:47 -0400

    add auth in post users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ import { auth } from '../middleware/auth.js';
 const userRouter = express.Router();
 userRouter.get('/', auth, getAll);
 userRouter.get('/:id', auth, getById);
-userRouter.post('/', post);
+userRouter.post('/', auth, post);
 userRouter.put('/:id', auth, put);
 userRouter.post('/login', login);
 userRouter.post('/loginrefresh', loginRefresh);
